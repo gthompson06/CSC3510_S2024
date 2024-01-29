2329d3ed589e4dabdb9ea2a44e098673b34fc460
David Lash <dlash02@gmail.com>
2024-01-28 20:55:42 -0600

    Update

---
+++ Week4/MainSystemPrep/DBUserExample.java
@@ -0,0 +1,15 @@
+package MainSystemPrep;
+
+import ModelPrep.DBAbstract;
+
+public class DBUserExample {
+    DBAbstract dbAbstract;
+
+    public DBUserExample(DBAbstract dbAbstract) {
+        this.dbAbstract = dbAbstract;
+    }
+    public int countCourseByDepartment( String dept ){
+        int count = 0;
+        return count;
+    }
+}
