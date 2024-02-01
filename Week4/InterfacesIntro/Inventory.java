dd74f479e08541c60269f3b1713cc876287ee85d
David Lash <dlash02@gmail.com>
2024-02-01 10:09:43 -0600

    update

--- Week4/InterfacesIntro/Inventory.java
+++ Week4/InterfacesIntro/Inventory.java
@@ -5,26 +5,21 @@ public class Inventory {
     private String item;
     private int ct;
     private String warehouseID;
-
     public Inventory(String id, String item, int ct, String warehouseID) {
         this.id = id;
         this.item = item;
         this.ct = ct;
         this.warehouseID = warehouseID;
     }
-
     public String getId() {
         return id;
     }
-
     public String getItem() {
         return item;
     }
-
     public int getCt() {
         return ct;
     }
-
     public String getWarehouseID() {
         return warehouseID;
     }
